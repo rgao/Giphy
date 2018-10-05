a4006039efcb1863507cd5e254058cd44f52b740
rgao <xinghoshi@gmail.com>
2018-10-04 23:36:35 -0700

    final version.1; still need to do download button

---
+++ README.md
@@ -0,0 +1,5 @@
+#Assignment 6: Gif Generator
+
+Task: create a gif generator website that pulls data from giphy.com. Gifs are displayed based on the button clicked and users can dynamically create buttons by inputing topics.
+
+Instructions reference: https://uclax.bootcampcontent.com/UCLA-Coding-Boot-Camp/UCLAWES201808FSF3/blob/master/Homework/Week-06/Instructions/homework_instructions.md
---
+++ assets/code.js
@@ -0,0 +1,88 @@
+$(document).ready(function() {
+
+    $("#gif-header").hide();
+
+    var topics = ["Squirrel", "Milky Way", "Dugtrio", "Game of Thrones"];
+
+    function makeBtn() {
+        $(".btn-display").empty();
+
+        for (i = 0; i < topics.length; i++) {
+            var btnDiv = $("<button>");
+            btnDiv.addClass("topic-btn btn btn-primary mx-1 mb-2");
+            btnDiv.attr("data-name", topics[i]);
+            btnDiv.text(topics[i]);
+            $(".btn-display").append(btnDiv);
+        };
+    };
+
+    $(document).on("click", ".topic-btn", function() {
+        $("#gif-header").show();
+        var topicName = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topicName + "&api_key=UZ25LXoQFwUYq1EepkpwCS2XICkZyWL9&limit=10";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).then(function(response) {
+            var result = response.data;
+            console.log(result);
+
+            for (j = 0; j < result.length; j++) {
+                var gifDiv = $("<div class='mx-auto my-2'>");
+                gifDiv.addClass("gif-container");
+
+                var image = $("<img>");
+                image.attr("src", result[j].images.fixed_height_still.url);
+                image.addClass("gif");
+                image.attr("data-static", result[j].images.fixed_height_still.url);
+                image.attr("data-motion", result[j].images.fixed_height.url);
+                image.attr("data-state", "static");
+
+                var gifTitle = $("<p class='mt-2'>").text(result[j].title);
+                var gifRating = $("<p class='mt-2' id='rating'>").text("Rating: " + result[j].rating);
+                var gifSource = $("<p id='source'>").text("Source: " + result[j].source_tld);
+
+                var dlDiv = $("<a>");
+                dlDiv.attr("href", result[j].images.fixed_height.url);
+                dlDiv.attr("download", true);
+                var dlBtn = $("<button class='btn fa fa-download btn-danger mb-2'>");
+                dlBtn.text(" Download");
+                dlDiv.append(dlBtn);
+
+                gifDiv.append(gifTitle);
+                gifDiv.append(image);
+                gifDiv.append(gifRating);
+                gifDiv.append(gifSource);
+                gifDiv.append(dlDiv);
+                $(".gif-display").prepend(gifDiv);
+            };
+        });
+    });
+
+    $("#add-btn").on("click", function() {
+        event.preventDefault();
+
+        var inputTopic = $("#btn-input").val();
+        if (inputTopic !== "") {
+            topics.push(inputTopic);
+            makeBtn();
+        $("#btn-input").val("");
+        };
+    });
+
+    $(document).on("click", ".gif", function() {
+        var state = $(this).attr("data-state");
+
+        if (state === "static") {
+            $(this).attr("src", $(this).attr("data-motion"));
+            $(this).attr("data-state", "motion");
+        } else {
+            $(this).attr("src", $(this).attr("data-static"));
+            $(this).attr("data-state", "static");
+        };
+    });
+
+    makeBtn();
+
+});
